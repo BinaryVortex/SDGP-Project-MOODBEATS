b20cc49a44a035411e2356a88573c4833a65c562
DinethJayasuriya <131681146+DinethJayasuriya@users.noreply.github.com>
2025-02-20 22:04:15 +0530

    Created the relevant page structure

---
+++ src/playlists.jsx
@@ -0,0 +1,50 @@
+
+<div class="playlist-container">
+  <header class="playlist-header">
+    <div class="cover-art">
+    <img src="album-cover.jpg" alt="Playlist Cover" />    
+    </div>
+    <div class="playlist-info">
+      <h1>Late Night Vibes</h1>
+      <p class="creator">Created by DJ Moonlight</p>
+      <p class="details">1 hr 23 min • 8 songs</p>
+      <div class="primary-controls">
+        <button class="play-button">Play</button>
+        <button class="shuffle-button">Shuffle</button>
+        <button class="repeat-button">Repeat</button>
+      </div>
+    </div>
+  </header>
+
+  <div class="track-list">
+    <div class="track-item">
+      <span class="track-number">1</span>
+      <div class="track-info">
+        <span class="track-title">Midnight Dreams</span>
+        <span class="track-artist">Luna Eclipse</span>
+      </div>
+      <span class="track-plays">2.3M</span>
+      <span class="track-duration">3:45</span>
+      <button class="like-button">Like</button>
+      <button class="more-button">More</button>
+    </div>
+
+  </div>
+
+
+  <div class="playback-controls">
+    <div class="now-playing">
+      <img src="current-track.jpg" alt="Now Playing"/>
+      <div class="track-info">
+        <span class="track-title">Midnight Dreams</span>
+        <span class="track-artist">Luna Eclipse</span>
+      </div>
+    </div>
+    <div class="player-controls">
+      <button class="prev-button">Previous</button>
+      <button class="play-button">Play</button>
+      <button class="next-button">Next</button>
+    </div>
+    <div class="additional-controls"></div>
+  </div>
+</div>
