d4d0e8427eb408cb23e11508dd6c11393ba44040
Bihandu Methsilu <bihandumethsilu55@gmail.com>
2025-02-21 13:07:36 +0530

    Search bar

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomePage from './src/Home';
 import OTPVerification from './src/OTP_pin';
+import SignInProfile from './src/Sign-in_profile';
 
 export default function App() {
   return (
