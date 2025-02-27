b8ca96f11ca891d81caa141ea3b298083598473d
Disandu Perera <152260753+BinaryVortex@users.noreply.github.com>
2025-02-27 11:24:05 +0530

    Revised Backend Logic For Database.

--- database.py
+++ database.py
@@ -1,22 +1,6 @@
-# Import necessary libraries for MongoDB connection
+# database.py
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.collection import Collection
+from .config import settings
 
-# MongoDB connection URL
-MONGO_DB_URL = "mongodb://localhost:27017"
-
-# Create a MongoDB client
-client = AsyncIOMotorClient(MONGO_DB_URL)
-
-# Select the database
-db = client["moodbeats"]
-
-# Functions to access collections
-def get_users_collection() -> Collection:
-    return db["users"]
-
-def get_playlists_collection() -> Collection:
-    return db["playlists"]
-
-def get_music_collection() -> Collection:
-    return db["music"]
+client = AsyncIOMotorClient(settings.MONGODB_URL)
+db = client[settings.DATABASE_NAME]
