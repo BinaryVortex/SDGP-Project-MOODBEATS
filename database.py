f14f0c5311d6c6664d89bbb4e9c981a8f68eb1d7
Disandu Perera <152260753+BinaryVortex@users.noreply.github.com>
2025-02-28 14:34:23 +0530

    Revised Backend Logic For The Database.

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 # database.py
 from motor.motor_asyncio import AsyncIOMotorClient
-from .config import settings
+from config import settings
 
 client = AsyncIOMotorClient(settings.MONGODB_URL)
 db = client[settings.DATABASE_NAME]
