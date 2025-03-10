b68ffc98bb57ba2e629169d95a21586786ad0212
Vihari Ambegoda <vihariambegodaa@gmail.com>
2025-03-10 21:31:46 +0530

    update app.js file

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,7 +1,49 @@
-// App.js
 import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MusicPlayer from './src/MusicPlayer';
+import LyricsPlayer from './src/LyricsPlayer';
 
-export default function App() {
-  return <AppNavigator />;
-}
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.container}>
+        <Stack.Navigator
+          initialRouteName="MusicPlayer"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#121212',
+              shadowColor: 'transparent',
+            },
+            headerTintColor: '#fff',
+            headerBackTitleVisible: false,
+          }}
+        >
+          <Stack.Screen
+            name="MusicPlayer"
+            component={MusicPlayer}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LyricsPlayer"
+            component={LyricsPlayer}
+            options={{ title: '', headerTransparent: true }}
+          />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+});
+
+export default App;
