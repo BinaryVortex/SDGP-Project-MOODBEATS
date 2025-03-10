cdaace602b03b481822633bdffab2abb6c8c9bf3
Dilukshan Sothyrajah <152638585+dilukshan12@users.noreply.github.com>
2025-03-10 22:39:08 +0530

    add the musicapp playlist base ui

--- MusicApp/App.js
+++ MusicApp/App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import MusicApp from './src/MusicApp';
+import MusicappPlaylist from './src/MusicappPlaylist';
 
 export default function App() {
   return (
-    <MusicApp/>
+    <MusicappPlaylist/>
   );
 }
 
