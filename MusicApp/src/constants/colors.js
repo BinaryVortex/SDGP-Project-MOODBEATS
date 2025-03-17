f7a4e1a26e2ab2733dd51e7386e6ea13e848255a
Dilukshan Sothyrajah <152638585+dilukshan12@users.noreply.github.com>
2025-03-17 23:54:57 +0530

    color.js setup in src/constants

---
+++ MusicApp/src/constants/colors.js
@@ -0,0 +1,24 @@
+// colors.js
+export default {
+    primary: '#5350C4',
+    primaryDark: '#3D39AC',
+    background: '#FFFFFF',
+    backgroundGradient: ['#EEEEFF', '#FFFFFF'],
+    messageText: '#000000',
+    messageBg: {
+      user: '#5350C4',
+      bot: '#F2F2FF',
+      error: '#FFF0F0',
+    },
+    messageText: {
+      user: '#FFFFFF',
+      bot: '#000000',
+      error: '#FF0000',
+    },
+    inputBorder: '#CCCCE5',
+    inputBorderFocus: '#5350C4',
+    icon: '#706DB0',
+    iconActive: '#3D39AC',
+    headerText: '#FFFFFF',
+    thinkingDot: '#6F6BC2',
+  };
