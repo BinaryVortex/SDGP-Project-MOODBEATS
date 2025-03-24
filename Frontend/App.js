d3923bc61cca0496a328e4c5c27dd752b953b47e
Vihari Ambegoda <vihariambegodaa@gmail.com>
2025-03-24 06:51:53 +0530

    packages deleted

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,49 +1,19 @@
 import React from 'react';
-import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MusicPlayer from './src/MusicPlayer';
-import LyricsPlayer from './src/LyricsPlayer';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Moodbasedsongs from '../src/Moodbasedsongs';
 
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" />
-      <SafeAreaView style={styles.container}>
-        <Stack.Navigator
-          initialRouteName="MusicPlayer"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#121212',
-              shadowColor: 'transparent',
-            },
-            headerTintColor: '#fff',
-            headerBackTitleVisible: false,
-          }}
-        >
-          <Stack.Screen
-            name="MusicPlayer"
-            component={MusicPlayer}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="LyricsPlayer"
-            component={LyricsPlayer}
-            options={{ title: '', headerTransparent: true }}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Moodbasedsongs />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#121212',
+    backgroundColor: 'white', // Ensure background is visible
   },
 });
-
-export default App;
